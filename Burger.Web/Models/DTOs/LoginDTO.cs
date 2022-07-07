a193eda3429611a397d3520c1053b7e378e9dfac
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-07-07 11:56:36 +0300

    loginDTO registerDTO classları acıldı

---
+++ Burger.Web/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Burger.Web.Models.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        [MinLength(5, ErrorMessage = "Email alanı en az 5 karakter olmalıdır.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
---
+++ Burger.Web/Models/DTOs/RegisterDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Burger.Web.Models.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string Ad { get; set; }
+        [Required]
+
+        public string Soyad { get; set; }
+        [Required]
+
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string RePassword { get; set; }
+
+        public bool IAgree { get; set; }
+    }
+}
